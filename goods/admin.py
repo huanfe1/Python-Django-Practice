4efa2845e241c30d36a6a19aaa086fa1246843c8
幻非 <eg1981400335@gmail.com>
2023-05-23 09:25:40 +0800

    admin 修改

--- goods/admin.py
+++ goods/admin.py
@@ -1,10 +1,29 @@
 from django.contrib import admin
 from goods.models import GoodsInfo
+from django.utils.html import mark_safe
 
 
-# Register your models here.
 class GoodsInfoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
+    def buttons(self, obj):
+        button_html = '''
+        <a class="changelink" href="http://127.0.0.1:8000/admin/goods/goodsinfo/%s/change/">编辑</a>
+        ''' % obj.id
+        return mark_safe(button_html)
+
+    buttons.short_description = '操作'
+
+    def good_img(self, obj):
+        img_html = '''
+        <img src="/%s" width="40px" height="40px">
+        ''' % obj.image
+        return mark_safe(img_html)
+
+    good_img.short_description = '商品图片'
+    list_display = ('id', 'name', 'price', 'isnew', 'buttons', 'good_img')
+    list_editable = ('price', 'isnew')
+    search_fields = ('name',)
 
 
 admin.site.register(GoodsInfo, GoodsInfoAdmin)
+admin.site.site_header = '商品信息管理后台'
+admin.site.site_title = '后台管理系统'
